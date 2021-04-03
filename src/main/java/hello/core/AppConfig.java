4627b59de3fcab3d1969a7afe635ebd1875091f8
tsseo <tsseo.dev@gmail.com>
2021-04-04 03:12:26 +0900

    AppConfig -> ApplicationContext(스프링 컨테이너)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,23 +8,35 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepositry;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    /**
+     * 메소드이름(memberService)을 Key
+     * 리턴되는 객체 인스턴스( new MemberServiceImpl(memberRepository()) )를 Value
+     * Key, Value로 스프링 컨테이너에 등록
+     * 꺼낼때는 메소드이름(Key)를 입력해서 사용
+     */
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepositry memberRepository() {
+    @Bean
+    public MemoryMemberRepositry memberRepository() {
         return new MemoryMemberRepositry();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPollicy());
     }
 
+    @Bean
     public DiscountPollicy discountPollicy() {
-//        return new FixDiscountPollicy();
         return new RateDiscountPolicy();
     }
 }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,11 +4,23 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        /**
+         * 스프링은 ApplicationContext로 시작!!
+         * 이게 스프링 컨테이너(-> @Configuration)
+         * 모든 객체(-> AppConfig.class 에서 @Bean 어노테이션)들을 관리함
+         */
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
