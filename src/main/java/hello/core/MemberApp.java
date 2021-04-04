04cf77856dee5599edaed46b14201632e5bea4eb
tsseo <tsseo.dev@gmail.com>
2021-04-04 21:50:07 +0900

    스프링 빈 조회 테스트코

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -9,8 +9,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class MemberApp {
     public static void main(String[] args) {
-//        AppConfig appConfig = new AppConfig();
-//        MemberService memberService = appConfig.memberService();
 
         /**
          * 스프링은 ApplicationContext로 시작!!
