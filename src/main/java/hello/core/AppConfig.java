8d762e2a5d43a0d1f94bf68619a22945be4cae5f
tsseo <tsseo.dev@gmail.com>
2021-04-06 13:50:39 +0900

    싱글톤 테스트코드

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -14,6 +14,13 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
     /**
+     * @Bean memberService -> new MemoryREpository()
+     * @Bean orderService -> new MemoryRepository()
+     * 결과적으로 각각 다른 2개의 'MemoryMemberRepository' 가 생성되면서 싱글톤이 깨지는 것 처럼 보임
+     * 스프링 컨테이너는 어떻게 해결하는지?
+     */
+
+    /**
      * 메소드이름(memberService)을 Key
      * 리턴되는 객체 인스턴스( new MemberServiceImpl(memberRepository()) )를 Value
      * Key, Value로 스프링 컨테이너에 등록
@@ -21,16 +28,19 @@ public class AppConfig {
      */
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemoryMemberRepositry memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepositry();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
---
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,48 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberService -> memberRepository = " + memberRepository1);
+        System.out.println("orderService -> memberRepository = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        Assertions.assertThat(memberRepository1).isSameAs(memberRepository2).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        /**
+         * @Configuration
+         * Expected : ~~AppConfig
+         * Actual   : ~~AppCOnfig$$~~~CGLIB
+         * 내가 만든 AppConfig 클래스가 아니라, 스프링이 CGLIB라는 바이트코드 조작 라이브러리를 사용해서
+         * AppConfig 클래스를 상속받은 임의의 다른 클래스를 만들고, 그 다른 클래스를 스프링 빈으로 등록
+         *      -> 그 임의의 다른 클래스가 싱글톤이 보장되도록 해줌
+         */
+        System.out.println("bean = " + bean.getClass());
+
+    }
+}
