b3d5abd3f3465d350b0b653e3c9eb66ae094507d
tsseo <tsseo.dev@gmail.com>
2021-04-10 23:40:09 +0900

    @Autowired 용도

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -9,8 +9,10 @@ public class MemberServiceImpl implements MemberService{
     private final MemberRepository memberRepository;
 
     /**
-     * @Component
-     * @ComponentScan 이 스캔 할 수 있도록(Bean 등록 시킴)
+     * @Autowired 어디에 쓰이나?
+     *
+     * @ComponentScan 이 스캔 할 때
+     * @Component 가 붙은 애들만 스캔하면서 빈 등록시킴
      * But, 아래 MemberServiceImpl 같은 경우 의존성 주입은 어떻게?
      *      -> @Autowired 로 해결(의존성 자동 주입)
      */
